1ede10bc0bf5a5665a6d081fab300d29ea003f74
alicemartins-hue <alicemartins7l@hotmail.com>
2025-09-04 18:53:24 +0100

    milestone activity done - a program to find the volume of a tire

---
+++ week1-milestone/tire_volume.py
@@ -0,0 +1,8 @@
+import math
+
+width = int(input("Enter the width of the tire in mm (ex 205): "))
+ratio = int(input("Enter the aspect ratio of the tire (ex 60): "))
+diameter = int(input("Enter the diamenter of the wheel in inches (ex 15): "))
+
+volume = (math.pi * (width **2) * ratio* (width * ratio + (2540*diameter)))/10000000000
+print(f"The aproximate volume is {volume:.2f} liters")
