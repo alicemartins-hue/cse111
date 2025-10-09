b6f9335dc7f37fdd5ca7e235548a23a883e652a4
alicemartins-hue <alicemartins7l@hotmail.com>
2025-10-09 15:57:36 +0100

    Add provinces data and implement changing_places function for province name replacement

---
+++ week5/week5-learn-activities/provinces.py
@@ -0,0 +1,30 @@
+def changing_places(list_name):
+    for i in range(len(list_name)):
+        if list_name[i] == "AB":
+            list_name[i] = "Alberta"
+    return list_name
+
+
+def read_list(filename):
+    text_list=[]
+
+    with open(filename,'rt') as text_file:
+        for line in text_file:
+            clean_line  = line.strip()
+            text_list.append(clean_line)
+    
+    return text_list
+
+def main():
+    text_list = read_list('provinces.txt')
+    changing_places(text_list)
+    x = 0
+    for i in range(len(text_list)):
+        if text_list[i] == "Alberta":
+            x+=1
+    print(text_list)
+    print(f"Alberta shows {x} times on the list.")
+
+if __name__ == "__main__":
+    main()
+
