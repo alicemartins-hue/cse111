79d4b3151880bc55b97e1661b1db6b2577286640
alicemartins-hue <alicemartins7l@hotmail.com>
2025-10-11 17:03:27 +0100

    Refactor main function to improve readability and remove unnecessary lines

--- week5/week5-learn-activities/provinces.py
+++ week5/week5-learn-activities/provinces.py
@@ -14,14 +14,18 @@ def read_list(filename):
             text_list.append(clean_line)
     
     return text_list
-
+ 
 def main():
     text_list = read_list('provinces.txt')
+    text_list.pop(0)
+    text_list.pop()
     changing_places(text_list)
+    
     x = 0
     for i in range(len(text_list)):
         if text_list[i] == "Alberta":
             x+=1
+
     print(text_list)
     print(f"Alberta shows {x} times on the list.")
 
