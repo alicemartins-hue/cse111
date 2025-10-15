26b38933099425f1567f3f5f1ce472b3143005e2
alicemartins-hue <alicemartins7l@hotmail.com>
2025-10-15 16:25:10 +0100

    Implement student information retrieval script with CSV reading functionality

---
+++ week5/week5-codealong/students.py
@@ -0,0 +1,33 @@
+import csv
+
+def read_dictionary(filename,key_column_index):
+    s_dictionary={}
+    with open(filename,'rt') as csvfile:
+        csvreader = csv.reader(csvfile,delimiter = ',')
+        next(csvreader)
+        for row in  csvreader:
+            key_value=row[key_column_index]
+            s_dictionary[key_value]=row
+    return s_dictionary
+
+
+def main():
+    KEY_INDEX=0
+    NAME_INDEX=1
+    students=read_dictionary("students.csv",KEY_INDEX)
+    inumber=input("Please enter a number: ")
+    inumber = inumber.replace(".","")
+    if not inumber.isdigit():
+        print("Invalid I-Number")
+    elif len(inumber) !=9:
+        print("An I-Number must be 9 digits long")
+    else:
+        if inumber in students:
+            student = students[inumber]
+            name= student[NAME_INDEX]
+            print(f"The student's name is {name}")
+        else:
+            print("no such student")
+
+if __name__ == "__main__":
+    main()
